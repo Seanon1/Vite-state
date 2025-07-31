5d49d787bb1335f4e1aa0d12a409b9cb1cb85f7e
Seanon1 <seangachango@gmail.com>
2025-07-31 12:43:50 +0300

    Counter effect

--- src/components/Effects.jsx
+++ src/components/Effects.jsx
@@ -1,19 +1,45 @@
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Timer() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setCount((count) => count + 1);
+//     }, 1000);
+//   }, []); // <- add empty brackets here
+
+//   return <h1>I've rendered {count} times!</h1>;
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Timer />);
+
+// export default Timer
+
+
 import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-function Timer() {
+function Counter() {
   const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  }, []);
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
 
-  return <h1>I've rendered {count} times!</h1>;
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Timer />);
+root.render(<Counter />);
 
-export default Timer
+export default Counter
